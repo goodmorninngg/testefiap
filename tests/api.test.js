100f74721c620398c257079c5abfb943c8e2ba5f
goodmorninngg <zer1nh233@gmail.com>
2024-11-02 11:24:20 -0300

    testandoattnova

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,28 +1,47 @@
 const request = require('supertest');
 const app = require('../app');
+const Ajv = require('ajv');
+const ajv = new Ajv();
+
+const loginSchema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "token": {
+      "type": "string"
+    }
+  },
+  "required": ["token"]
+};
 
 describe('Teste da API de Login', () => {
   it('Deve retornar status 200 e token para credenciais válidas', async () => {
     const res = await request(app)
       .post('/login')
       .send({ email: 'email@algumacoisa.com', senha: 'senhaaa12312' });
+    
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('token');
+    
+    const validate = ajv.compile(loginSchema);
+    const valid = validate(res.body);
+    
+    expect(valid).toBe(true);
+    if (!valid) console.log(validate.errors);
   });
 
   it('Deve retornar status 400 para campo de e-mail vazio', async () => {
     const res = await request(app)
       .post('/login')
       .send({ email: '', senha: 'senhaaa12312' });
+    
     expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('error', 'O campo de e-mail não pode estar vazio.');
   });
 
   it('Deve retornar status 400 para e-mail em formato incorreto', async () => {
     const res = await request(app)
       .post('/login')
       .send({ email: 'emailincorreto', senha: '123' });
+    
     expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('error', 'Formato de e-mail inválido.');
   });
 });
